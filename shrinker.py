a1d3a893cbe86e67c31d34ade85cbd46f3c76370
Viktor Shevtsov <viktross736@hotmail.com>
2022-01-22 00:34:14 +0300

    Refactored again LMFAO

--- shrinker.py
+++ shrinker.py
@@ -12,74 +12,94 @@ def get_file_path():
     return sys.argv[1]
 
 
-def get_file_name():
-
-    return sys.argv[1].split('\\')[-1]
-
-
-def get_file_endings(file_name):
-    file_names_dict = {
-        'video_only': f'{file_name + "_NO_AUD.mp4"}',
-        'audio_only': f'{file_name + "_NO_VID.mp4"}',
-        'rescaled': f'{file_name + "_RESCALED.mp4"}',
-        'changed_bitrate': f'{file_name + "_CHANGED_BITRATE.mp4"}',
-        '8mb': '_8MB.mp4',
-        'original': file_name
-    }
-
-    return file_names_dict
-
-
-def check_file_size(video_data, audio_data):
-    if int(video_data['format']['size']) + int(audio_data['format']['size']) < FILE_SIZE_LIMIT_IN_BYTES:
-        print('\n\nFile is already under 8MB!')
-        time.sleep(2)
-        sys.exit(0)
-        
-
-def split_video(file_path, output):
-    subprocess.run(f'.\\ffmpeg.exe -i "{file_path}" -y -c:v copy -an ".\\tmp\\{output["video_only"]}" -c:a copy -vn ".\\tmp\\{output["audio_only"]}"')
-
-
-def get_data(file_name):
-    stdout = subprocess.run(f'.\\ffprobe.exe -i ".\\tmp\\{file_name}" -v quiet -print_format json -show_streams -show_format', capture_output=True, encoding='UTF-8').stdout
+def get_data(file_path):
+    stdout = subprocess.run(f'.\\ffprobe.exe -i "{file_path}" -v quiet -print_format json -show_streams -show_format', capture_output=True, encoding='UTF-8').stdout
     data_json = json.loads(stdout)
-
+    
     return data_json
 
 
-def rescale_video(file_name, output, video_data):
-    if {video_data['streams'][0]['width'], video_data['streams'][0]['height']}.issubset({1920, 1080}):
-        subprocess.run(f'.\\ffmpeg.exe -i ".\\tmp\\{file_name}" -y -s 1280x720 ".\\tmp\\{output}"')
+def check_file_size(file_path):
+    data = get_data(file_path)
+    if int(data['format']['size']) < FILE_SIZE_LIMIT_IN_BYTES:
+        return '<8mb'
+
+    return '>8mb'
 
 
-def determine_target_bitrate(video_data, audio_data):
-    reduction_coeff = int(video_data['format']['size']) / (FILE_SIZE_LIMIT_IN_BYTES - int(audio_data['format']['size']))
-    target_bitrate = int(video_data['streams'][0]['bit_rate']) / math.ceil(reduction_coeff)
+def split_video(file_path, output_path, output_file_video, output_file_audio):
+    subprocess.run(f'.\\ffmpeg.exe -i "{file_path}" -y -c:v copy -an "{output_path + output_file_video}" -c:a copy -vn "{output_path + output_file_audio}"')
+
+
+def check_video_resolution(file_path):
+    video_data = get_data(file_path)
+    if {int(video_data['streams'][0]['width']), int(video_data['streams'][0]['height'])}.issubset({1920, 1080}):
+        return True
+    
+    return False
+        
+
+def rescale_video(file_path, output_path, output_file_name, target_resolution):
+    is_video_1080 = check_video_resolution(file_path)
+    if is_video_1080:
+        subprocess.run(f'.\\ffmpeg.exe -i "{file_path}" -y -s {target_resolution} "{output_path + output_file_name}"')
+        return True
+    
+    return False
+
+
+def merge_video(file_path_1, file_path_2, output_path, output_file_name):
+    subprocess.run(f'.\\ffmpeg.exe -i "{file_path_1}" -i "{file_path_2}" -y -c copy "{output_path + output_file_name}"')
+
+
+def determine_target_bitrate(video_file_path, audio_file_path):
+    video_data = get_data(video_file_path)
+    audio_data = get_data(audio_file_path)
+    reduction_coefficient = int(video_data['format']['size']) / (FILE_SIZE_LIMIT_IN_BYTES - int(audio_data['format']['size']))
+    target_bitrate = int(video_data['streams'][0]['bit_rate']) / math.ceil(reduction_coefficient)
 
     return target_bitrate
 
 
-def process_video(file_names, video_data, audio_data, bitrate, output, output_path):
-    if not video_data: #JSON is empty = video was not rescaled (less than 1080p) -> Change bitrate instead of rescaling.
-        subprocess.run(f'.\\ffmpeg.exe -i ".\\tmp\\{file_names["video_only"]}" -y -b:v {bitrate} ".\\tmp\\{file_names["changed_bitrate"]}"')
-        subprocess.run(f'.\\ffmpeg.exe -i ".\\tmp\\{file_names["changed_bitrate"]}" -i ".\\tmp\\{file_names["audio_only"]}" -y -c copy "{output_path + output}"')
-    elif video_data: #JSON isn't empty = video was rescaled -> Check if it's under 8mb -> If it isn't, change bitrate.
-        if int(video_data['format']['size']) + int(audio_data['format']['size']) > FILE_SIZE_LIMIT_IN_BYTES:
-            subprocess.run(f'.\\ffmpeg.exe -i ".\\tmp\\{file_names["video_only"]}" -y -b:v {bitrate} ".\\tmp\\{file_names["changed_bitrate"]}"')
-            subprocess.run(f'.\\ffmpeg.exe -i ".\\tmp\\{file_names["changed_bitrate"]}" -i ".\\tmp\\{file_names["audio_only"]}" -y -c copy "{output_path + output}"')
-        else:
-            subprocess.run(f'.\\ffmpeg.exe -i ".\\tmp\\{file_names["rescaled"]}" -i ".\\tmp\\{file_names["audio_only"]}" -y -c copy "{output_path + output}"')
+def change_bitrate(file_path, output_path, output_file, target_bitrate):
+    subprocess.run(f'.\\ffmpeg.exe -i "{file_path}" -y -b:v {target_bitrate} -maxrate:v {target_bitrate} -bufsize 4M "{output_path + output_file}"')
 
 
-file_path = get_file_path()
-file_name = get_file_name()
-file_names = get_file_endings(file_name)
-split_video(file_path, file_names)
-video_data = get_data(file_names['video_only'])
-audio_data = get_data(file_names['audio_only'])
-check_file_size(video_data, audio_data)
-rescale_video(file_names['video_only'], file_names['rescaled'], video_data)
-rescaled_video_data = get_data(file_names['rescaled'])
-bitrate = determine_target_bitrate(video_data, audio_data)
-process_video(file_names, rescaled_video_data, audio_data, bitrate, file_names['8mb'], file_path)
+def return_compressed_video(file_path, output_path, output_file_name):
+    subprocess.run(f'.\\fmpeg.exe -i "{file_path}" -y -c copy "{output_path + output_file_name}"')
+
+
+def main():
+    original_file_path = get_file_path()
+
+    try:
+        if check_file_size(original_file_path) == '<8mb':
+            print('\nFILE IS ALREADY UNDER 8MB!')
+            time.sleep(2)
+            sys.exit(0)
+
+    except KeyError as e:
+        print('\nINVALID FILE FORMAT.')
+        time.sleep(2)
+        sys.exit(1)
+
+    split_video(original_file_path, '.\\tmp\\', 'video_only.mp4', 'audio_only.mp4')
+
+    was_rescaled = rescale_video('.\\tmp\\video_only.mp4', '.\\tmp\\', 'rescaled_video.mp4', '1680x1050')
+
+    if was_rescaled is False:
+        merge_video('.\\tmp\\video_only.mp4', '.\\tmp\\audio_only.mp4', '.\\tmp\\', 'merged.mp4')
+    else:
+        merge_video('.\\tmp\\rescaled_video.mp4', '.\\tmp\\audio_only.mp4', '.\\tmp\\', 'merged.mp4')
+
+    if check_file_size('.\\tmp\\merged.mp4') == '>8mb' and was_rescaled:
+        target_bitrate = determine_target_bitrate('.\\tmp\\rescaled_video.mp4', '.\\tmp\\audio_only.mp4')
+        change_bitrate('.\\tmp\\merged.mp4', original_file_path, '_8mb.mp4', target_bitrate)
+    elif check_file_size('.\\tmp\\merged.mp4') == '>8mb':
+        target_bitrate = determine_target_bitrate('.\\tmp\\video_only.mp4', '.\\tmp\\audio_only.mp4')
+        change_bitrate('.\\tmp\\merged.mp4', original_file_path, '_8mb.mp4', target_bitrate)
+    else:
+        return_compressed_video('.\\tmp\\rescaled_video.mp4', original_file_path, '_8mb.mp4')
+
+
+main()
